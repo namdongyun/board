94ff8cefd36b7c39c1ec8a431195683fc6f84aba
ndy <dynam99@naver.com>
2023-11-17 22:15:27 +0900

    11

---
+++ src/main/java/com/example/board/controller/boardController.java
@@ -0,0 +1,12 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class boardController {
+
+
+}
